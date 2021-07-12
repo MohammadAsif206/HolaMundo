2f7af198efa1fe933c34bacea9081b3b3df60f56
MohammadAsif206 <37873675+MohammadAsif206@users.noreply.github.com>
2021-07-12 16:09:10 -0500

    Update App.java

--- src/main/java/dev/mohammad/app/App.java
+++ src/main/java/dev/mohammad/app/App.java
@@ -8,7 +8,8 @@ public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> config.enableCorsForAllOrigins());
         app.get("/hola", context -> {
-            context.result("HOLA/HELLO/SALAM this many requests have been handled by this server, version 12.00 with a webhook able to automatically build successfully perfectly fine "+ ++App.counter);
+            context.result("HOLA/HELLO/SALAM this many requests have been handled by this server"+
+                           "version 12.00 with a webhook able to automatically build successfully perfectly fine. The issue fixed, I will share with you what was wrong "+ ++App.counter);
 
         });
         app.start();
